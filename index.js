9bcdb175556d3e9db27b63c6dae8e38eaed3f907
Sanskritivv <sanskriti09v@gmail.com>
2025-02-28 11:09:43 +0530

    idk whats this commit

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-const jsonfile = require('jsonfile');
-const moment = require('moment');
-const simpleGit =require('simple-git');
-const random = require('random');
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
 
-const FILE_PATH = './data.json';
-
-const makeCommit = n =>{
-    if (n===0) return simpleGit().push();
-    const x = Math.floor(Math.random()*55);
-    const y = Math.floor(Math.random()*7);
-    const DATE = moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format();
-    const data={
-        data: DATE
-    }
-
-    console.log(DATE);
-    jsonfile.writeFile(FILE_PATH, data, ()=>{ 
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--data':DATE},
-        makeCommit.bind(this,--n));
-        });
+// Check if current date is after February 28, 2025
+const targetDate = moment("2025-02-28", "YYYY-MM-DD");
+if (!moment().isAfter(targetDate)) {
+  console.log("Current date is not after 28-02-2025. Code will not run.");
+  process.exit(0);
 }
-makeCommit(500);
+
+const path = "./data.json";
+
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = { date };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
+};
+
+const makeCommits = (n) => {
+  if (n === 0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = { date };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, makeCommits.bind(this, --n));
+  });
+};
+
+makeCommits(100);
